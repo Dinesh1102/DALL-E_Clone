938a8461c27a486be4c71950aea626ec2297ca5d
Dinesh1102 <94825630+Dinesh1102@users.noreply.github.com>
2023-07-05 20:48:38 +0530

    Update FormField.jsx

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -12,7 +12,7 @@ const FormField = ({labelName,type,name,placeholder,value,handleChange,isSurpris
         )}
       </div>
       <input type={type} id={name} name={name} placeholder={placeholder} value={value} onChange={handleChange} required 
-      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'/>
+      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#000000] focus:border-[#000000] outline-none block w-full p-3'/>
     </div>
     
   )
