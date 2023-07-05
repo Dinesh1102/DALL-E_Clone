032fd266bbc3b2d0fca03372a2736b49242e0792
Dinesh1102 <94825630+Dinesh1102@users.noreply.github.com>
2023-07-05 20:50:59 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {logo} from './assets';
+import {logox} from './assets';
 import {BrowserRouter,Link,Route,Routes} from 'react-router-dom';
 import { Home, CreatePost} from './pages';
 
@@ -7,9 +8,14 @@ import { Home, CreatePost} from './pages';
 const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between bg-white items-center sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      <header className='w-full flex text-bold justify-between bg-white items-center sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to="/">
-          <img src={logo} alt="logo" className='w-28 object-contain' />
+          <div className='flex'>
+            <img src={logox} alt="logo" className='w-10 object-contain float:left' />
+            <div className='float:left'>
+              <h2 className='px-4 py-4 text-bold'>DALL-E 2.0</h2>
+            </div>
+          </div>
         </Link>
         <Link to="/create-post" className='font-inter font-medium bg-[#2D4356] text-white px-4 py-2 rounded-md'>+ New Post</Link>
       </header>
