04c54decbdc103d1a46f22706190a62381e67ddc
Dinesh1102 <94825630+Dinesh1102@users.noreply.github.com>
2023-07-05 20:51:29 +0530

    Update index.js

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,10 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
-
+import logox from './logox.png';
 export {
   download,
   logo,
   preview,
+  logox,
 };
